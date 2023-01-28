0320827ed08270cc8f2f2447f62612ae943ce574
Linh Dinh <nhlinh1803@gmail.com>
2023-01-28 11:12:15 -0800

    Completed Java Assignment 002

--- src/ArchimedesPiMethod.java
+++ src/ArchimedesPiMethod.java
@@ -1,7 +1,36 @@
+
+/**
+ *
+ * @author Trevor Hartman
+ * @author Linh Dinh
+ *
+ * @since Version 1.0
+ *
+ */
+
 import java.util.Scanner;
 
 public class ArchimedesPiMethod {
     public static void main(String[] args) {
+        while (true){
+            System.out.println("Please type the number of sides: ");
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            if(n<1){
+                break;
+            }
+            double b = 360.0/n;
+            double a = b/2.0;
+            double s = 2.0 * Math.toRadians(Math.sin(a));
+            double p = n * s;
+            double pi = p/2.0;
+            System.out.printf("Our Pi estimate is: %.10f%n",pi);
+            System.out.println(pi);
+        }
+
+
+
+
 
     }
 }
